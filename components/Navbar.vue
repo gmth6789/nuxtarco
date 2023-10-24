0b2dc090244c28e1e14fe54d7b2e1f9624c0f78c
gmth6789 <gmth.6789@gmail.com>
2023-10-24 11:23:31 +0700

    origin

---
+++ components/Navbar.vue
@@ -0,0 +1,259 @@
+<template>
+  <div class="navbar">
+  <div id="u1169" class="ax_default" data-label="navbar">
+          <div id="u1169_state0" class="panel_state" data-label="Login_admin" style="">
+            <div id="u1169_state0_content" class="panel_state_content">
+
+              <!-- Logo (Dynamic Panel) -->
+              <div id="u1170" class="ax_default" data-label="Logo">
+                <div id="u1170_state0" class="panel_state" data-label="State1" style="">
+                  <div id="u1170_state0_content" class="panel_state_content">
+
+                    <!-- Arco (Group) -->
+                    <div id="u1171" class="ax_default" data-label="Arco" data-left="3" data-top="7" data-width="26" data-height="19" layer-opacity="1">
+
+                      <!-- 编组备份 2 1 (Dynamic Panel) -->
+                      <div id="u1172" class="ax_default" data-label="编组备份 2 1">
+                        <div id="u1172_state0" class="panel_state" data-label="State1" style="">
+                          <div id="u1172_state0_content" class="panel_state_content">
+
+                            <!-- 页面1 (Group) -->
+                            <div id="u1173" class="ax_default" data-label="页面1" data-left="-1.8137084989847598" data-top="-2.0710678118654764" data-width="29.62741699796952" data-height="22.884776310850242" layer-opacity="1">
+
+                              <!-- 画板 (Group) -->
+                              <div id="u1174" class="ax_default" data-label="画板" data-left="-1.8137084989847598" data-top="-2.0710678118654764" data-width="29.62741699796952" data-height="22.884776310850242" layer-opacity="1">
+
+                                <!-- 编组 6备份 (Group) -->
+                                <div id="u1175" class="ax_default" data-label="编组 6备份" data-left="-1.8137084989847598" data-top="-2.0710678118654764" data-width="29.62741699796952" data-height="22.884776310850242" layer-opacity="1">
+
+                                  <!-- 编组 5备份 (Group) -->
+                                  <div id="u1176" class="ax_default" data-label="编组 5备份" data-left="-1.8137084989847598" data-top="-2.0710678118654764" data-width="29.62741699796952" data-height="22.884776310850242" layer-opacity="1">
+
+                                    <!-- 编组 3 (Group) -->
+                                    <div id="u1177" class="ax_default" data-label="编组 3" data-left="-1.8137084989847598" data-top="-2.0710678118654764" data-width="29.62741699796952" data-height="22.884776310850242" layer-opacity="1">
+
+                                      <!-- 编组备份 2 (Group) -->
+                                      <div id="u1178" class="ax_default" data-label="编组备份 2" data-left="-1.8137084989847598" data-top="-2.0710678118654764" data-width="29.62741699796952" data-height="22.884776310850242" layer-opacity="1">
+
+                                        <!-- 矩形备份 3 (Shape) -->
+                                        <div id="u1179" class="ax_default shape" data-label="矩形备份 3">
+                                          <img id="u1179_img" class="img " src="images/admin/矩形备份_3_u1179.svg"/>
+                                          <div id="u1179_text" class="text " style="display:none; visibility: hidden">
+                                            <p></p>
+                                          </div>
+                                        </div>
+
+                                        <!-- 矩形备份 7 (Shape) -->
+                                        <div id="u1180" class="ax_default shape" data-label="矩形备份 7">
+                                          <img id="u1180_img" class="img " src="images/admin/矩形备份_7_u1180.svg"/>
+                                          <div id="u1180_text" class="text " style="display:none; visibility: hidden">
+                                            <p></p>
+                                          </div>
+                                        </div>
+
+                                        <!-- 矩形 (Shape) -->
+                                        <div id="u1181" class="ax_default shape" data-label="矩形">
+                                          <img id="u1181_img" class="img " src="images/admin/矩形_u1181.svg"/>
+                                          <div id="u1181_text" class="text " style="display:none; visibility: hidden">
+                                            <p></p>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <!-- Unnamed (Rectangle) -->
+                    <div id="u1182" class="ax_default shape">
+                      <div id="u1182_div" class=""></div>
+                      <div id="u1182_text" class="text ">
+                        <p><span>GMTH</span></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- navbar- right (Group) -->
+              <div id="u1183" class="ax_default" data-label="navbar- right" data-left="915" data-top="14" data-width="489" data-height="32" layer-opacity="1">
+
+                <!-- search-box (Group) -->
+                <div id="u1184" class="ax_default" data-label="search-box" data-left="915" data-top="14" data-width="220" data-height="32" layer-opacity="1">
+
+                  <!-- search-box (Rectangle) -->
+                  <div id="u1185" class="ax_default shape" data-label="search-box">
+                    <div id="u1185_div" class=""></div>
+                    <div id="u1185_text" class="text " style="display:none; visibility: hidden">
+                      <p></p>
+                    </div>
+                  </div>
+
+                  <!-- wrapper (Dynamic Panel) -->
+                  <div id="u1186" class="ax_default" data-label="wrapper">
+                    <div id="u1186_state0" class="panel_state" data-label="State1" style="">
+                      <div id="u1186_state0_content" class="panel_state_content">
+
+                        <!-- placeholder (Rectangle) -->
+                        <div id="u1187" class="ax_default shape" data-label="placeholder">
+                          <div id="u1187_div" class=""></div>
+                          <div id="u1187_text" class="text ">
+                            <p><span>Input content</span></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- addAfter (Group) -->
+                  <div id="u1188" class="ax_default" data-label="addAfter" data-left="1110" data-top="24" data-width="11" data-height="11" layer-opacity="1">
+
+                    <!-- interactive-button/search (Group) -->
+                    <div id="u1189" class="ax_default" data-label="interactive-button/search" data-left="1110" data-top="24" data-width="11" data-height="11" layer-opacity="1">
+
+                      <!-- Vector (Shape) -->
+                      <div id="u1190" class="ax_default shape" data-label="Vector">
+                        <img id="u1190_img" class="img " src="images/admin/vector_u1190.svg"/>
+                        <div id="u1190_text" class="text " style="display:none; visibility: hidden">
+                          <p></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Button (Group) -->
+                <div id="u1191" class="ax_default" data-label="Button" data-left="1162" data-top="14" data-width="32" data-height="32" layer-opacity="1">
+
+                  <!-- Button (Rectangle) -->
+                  <div id="u1192" class="ax_default shape" data-label="Button">
+                    <div id="u1192_div" class=""></div>
+                    <div id="u1192_text" class="text " style="display:none; visibility: hidden">
+                      <p></p>
+                    </div>
+                  </div>
+
+                  <!-- general/notification (Group) -->
+                  <div id="u1193" class="ax_default" data-label="general/notification" data-left="1173" data-top="24" data-width="11" data-height="12" layer-opacity="1">
+
+                    <!-- Vector (Shape) -->
+                    <div id="u1194" class="ax_default shape" data-label="Vector">
+                      <img id="u1194_img" class="img " src="images/admin/vector_u1194.svg"/>
+                      <div id="u1194_text" class="text " style="display:none; visibility: hidden">
+                        <p></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Button (Group) -->
+                <div id="u1195" class="ax_default" data-label="Button" data-left="1216" data-top="14" data-width="32" data-height="32" layer-opacity="1">
+
+                  <!-- Button (Rectangle) -->
+                  <div id="u1196" class="ax_default shape" data-label="Button">
+                    <div id="u1196_div" class=""></div>
+                    <div id="u1196_text" class="text " style="display:none; visibility: hidden">
+                      <p></p>
+                    </div>
+                  </div>
+
+                  <!-- general/moon-fill (Group) -->
+                  <div id="u1197" class="ax_default" data-label="general/moon-fill" data-left="1226" data-top="25" data-width="11" data-height="11" layer-opacity="1">
+
+                    <!-- Vector (Shape) -->
+                    <div id="u1198" class="ax_default shape" data-label="Vector">
+                      <img id="u1198_img" class="img " src="images/admin/vector_u1198.svg"/>
+                      <div id="u1198_text" class="text " style="display:none; visibility: hidden">
+                        <p></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Button (Group) -->
+                <div id="u1199" class="ax_default" data-label="Button" data-left="1268" data-top="14" data-width="32" data-height="32" layer-opacity="1">
+
+                  <!-- Button (Rectangle) -->
+                  <div id="u1200" class="ax_default shape" data-label="Button">
+                    <div id="u1200_div" class=""></div>
+                    <div id="u1200_text" class="text " style="display:none; visibility: hidden">
+                      <p></p>
+                    </div>
+                  </div>
+
+                  <!-- interactive-button/settings (Group) -->
+                  <div id="u1201" class="ax_default" data-label="interactive-button/settings" data-left="1277" data-top="23" data-width="14" data-height="14" layer-opacity="1">
+
+                    <!-- interactive-button/settings (Rectangle) -->
+                    <div id="u1202" class="ax_default shape" data-label="interactive-button/settings">
+                      <div id="u1202_div" class=""></div>
+                      <div id="u1202_text" class="text " style="display:none; visibility: hidden">
+                        <p></p>
+                      </div>
+                    </div>
+
+                    <!-- Vector (Shape) -->
+                    <div id="u1203" class="ax_default shape" data-label="Vector">
+                      <img id="u1203_img" class="img " src="images/admin/vector_u1203.svg"/>
+                      <div id="u1203_text" class="text " style="display:none; visibility: hidden">
+                        <p></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Button (Group) -->
+                <div id="u1204" class="ax_default" data-label="Button" data-left="1320" data-top="14" data-width="32" data-height="32" layer-opacity="1">
+
+                  <!-- Button (Rectangle) -->
+                  <div id="u1205" class="ax_default shape" data-label="Button">
+                    <div id="u1205_div" class=""></div>
+                    <div id="u1205_text" class="text " style="display:none; visibility: hidden">
+                      <p></p>
+                    </div>
+                  </div>
+
+                  <!-- general/skin (Group) -->
+                  <div id="u1206" class="ax_default" data-label="general/skin" data-left="1330" data-top="24" data-width="12" data-height="11" layer-opacity="1">
+
+                    <!-- Vector (Shape) -->
+                    <div id="u1207" class="ax_default shape" data-label="Vector">
+                      <img id="u1207_img" class="img " src="images/admin/vector_u1207.svg"/>
+                      <div id="u1207_text" class="text " style="display:none; visibility: hidden">
+                        <p></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Avatar (Group) -->
+                <div id="u1208" class="ax_default" data-label="Avatar" data-left="1372" data-top="14" data-width="32" data-height="32" layer-opacity="1">
+
+                  <!-- Avatar (Rectangle) -->
+                  <div id="u1209" class="ax_default shape" data-label="Avatar">
+                    <div id="u1209_div" class=""></div>
+                    <div id="u1209_text" class="text " style="display:none; visibility: hidden">
+                      <p></p>
+                    </div>
+                  </div>
+
+                  <!-- placeholder  (Dynamic Panel) -->
+                  <div id="u1210" class="ax_default" data-label="placeholder ">
+                    <div id="u1210_state0" class="panel_state" data-label="State1" style="">
+                      <div id="u1210_state0_content" class="panel_state_content">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+</div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <Nuxt />
+  <Navbar />
+    <slot />
   </div>
 </template>
 
